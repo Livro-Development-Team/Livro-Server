b20c4def28a8dc9c4eba682148ce4b026ef00581
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-12-02 20:07:42 +0900

    Feature access token 생성 구현

---
+++ src/utils/mkToken.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+const mkAccess = async (uuid, admin, secret) => {
+	const token = await jwt.sign(
+		{
+			uuid,
+			admin,
+		},
+		secret,
+		{
+			expiresIn: '50000m',
+		},
+	);
+	return token;
+};
+
+module.exports = mkAccess;
