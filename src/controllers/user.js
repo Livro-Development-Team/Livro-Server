b3cb0f18740972c6617076281984a7def8e157fb
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-12-02 20:22:13 +0900

    Feature register, login

---
+++ src/controllers/user.js
@@ -0,0 +1,23 @@
+const { registerUser, findUser } = require('../services/user');
+
+const register = async (req, res, next) => {
+	await registerUser(req.body);
+	res.status(201).end();
+};
+
+async function login(req, res, next) {
+	const token = await findUser(req.body);
+	res.status(200).json({
+		accessToken: token,
+	});
+}
+
+const getLoanList = (req, res, next) => {
+	return;
+};
+
+const getUserInfo = (req, res, next) => {
+	return;
+};
+
+module.exports = (register, login, getLoanList, getUserInfo);
