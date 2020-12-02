ded67bb698c58a4cdfb4210b21b41d7770775328
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-12-02 20:12:21 +0900

    Fix sequelize 적용 오류 수정

--- src/app.js
+++ src/app.js
@@ -3,40 +3,51 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 // Set Router
-var routes = require('./routes')
+var routes = require('./routes');
 
 // Set Sequelize
-const { sequelize } = require('./config/config')
+const sequelize = require('./config/config').sequelize;
 
 var app = express();
-sequelize.sync();
+sequelize
+	.sync()
+	.then(() => {
+		console.log('DB connection success');
+	})
+	.catch((err) => {
+		console.log('connect failed...');
+		console.log(err);
+	});
 
 // view engine setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+// set jwt secret key
+app.set('jwt-secret', process.env.JWT_SECRET_KEY);
 
 // Route handler
-app.use('/', routes)
+app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((err, req, res, next) => {
+	res.status(404).json({
+		message: err.message,
+	});
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.json({ error: 'error', message: err });
 });
 
-module.exports = app
+module.exports = app;
