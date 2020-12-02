f6014bbcf0d50a0ba6bc816ef52d468836a1d400
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-12-02 20:22:22 +0900

    ETC

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
+const admin = require('../controllers/admin');
 
-router.post('/auth', ) //admin login
-router.post('/notice', ) //write notice
-router.put('/notice/:id', ) //update notice
-router.delete('/notice/:id', ) //delete notice
-router.get('/loan', ) //get loan list
-router.get('/loan/:id', ) //return book
-router.get('/loan/delay', ) //get delay list
+router.post('/auth'); //admin login
+router.post('/notice'); //write notice
+router.put('/notice/:id'); //update notice
+router.delete('/notice/:id'); //delete notice
+router.get('/loan'); //get loan list
+router.get('/loan/:id'); //return book
+router.get('/loan/delay'); //get delay list
 
 module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
-const user = require('./users')
-const admin = require('./admin')
-const notice = require('./notice')
-const book = require('./book')
+const user = require('./user');
+const admin = require('./admin');
+const notice = require('./notice');
+const book = require('./book');
 
-router.use("/user", user)
-router.use("/admin", admin)
-router.use("/notice", notice)
-router.use("/book", book)
+router.use('/user', user);
+router.use('/admin', admin);
+router.use('/notice', notice);
+router.use('/book', book);
 
 module.exports = router;
