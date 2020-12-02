b123dc1f5a7ec90d7e3bb7d4fe0aa5a93dd526fa
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-12-02 20:09:07 +0900

    Refactor sequelize console logging set false

--- src/config/config.js
+++ src/config/config.js
@@ -4,37 +4,38 @@ var config = require('./database');
 const db = {};
 
 var sequelize = new Sequelize(
-    config.database.database,
-    config.database.user,
-    config.database.password,
-    {
-        host: config.database.host,
-        dialect: "mysql",
-        define: {
-            timestamps: false,
-        },
-        timezone: "+09:00",
-        dialectOptions: {
-            dateStrings: true,
-            typeCast: true,
-        },
-        pool: {
-            max: 30,
-            min: 0,
-            acquire: 30000,
-            idle: 10000,
-        },
-    },
+	config.database.database,
+	config.database.user,
+	config.database.password,
+	{
+		host: config.database.host,
+		dialect: 'mysql',
+		define: {
+			timestamps: false,
+		},
+		timezone: '+09:00',
+		dialectOptions: {
+			dateStrings: true,
+			typeCast: true,
+		},
+		pool: {
+			max: 30,
+			min: 0,
+			acquire: 30000,
+			idle: 10000,
+		},
+		logging: false,
+	},
 );
 
-db.sequelize = sequelize
-db.Sequelize = Sequelize
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 // Import model function from ../models/...js
-var User = require('../models/user')
-var Loan = require('../models/loan')
-var Notice = require('../models/notice')
-var Book = require('../models/book')
+var User = require('../models/user');
+var Loan = require('../models/loan');
+var Notice = require('../models/notice');
+var Book = require('../models/book');
 
 // Add model
 db.User = User(sequelize, Sequelize);
--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,15 @@
-var path = require('path')
-require('dotenv').config({ path: path.join(__dirname, '..', '.env' )})
+var path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 
 const config = {
-    test_module: false,
-    database: {
-        host: process.env.DB_HOST,
-        port: 3306,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DATABASE,
-    }
-}
+	test_module: false,
+	database: {
+		host: process.env.DB_HOST,
+		port: 3306,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		database: process.env.DATABASE,
+	},
+};
 
-module.exports = config
+module.exports = config;
