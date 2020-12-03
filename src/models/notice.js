924e530506aed12c01985c1ce33a5532cf1106ff
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-12-03 19:06:21 +0900

    ETC

--- src/models/notice.js
+++ src/models/notice.js
@@ -1,37 +1,40 @@
 const Notice = (sequelize, DataTypes) => {
-    const notice = sequelize.define('notice', {
-        uuid : {
-            type: DataTypes.STRING(50),
-            primaryKey: true,
-        },
-        title : {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-        },
-        content : {
-            type: DataTypes.STRING(500),
-        },
-        userUuid : {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-            field: "user_uuid",
-        },
-        school : {
-            type: DataTypes.STRING(20),
-            allowNull: false,
-        },
-        createdAt : {
-            type: DataTypes.DATE,
-            defaultValue: DataTypes.NOW,
-            field: "created_at"
-        },
-    },
-    {
-        charset: 'utf8',
-        freezeTableName: true,
-        tableName: 'notice',
-    });
-    return notice
-}
+	const notice = sequelize.define(
+		'notice',
+		{
+			uuid: {
+				type: DataTypes.STRING(50),
+				primaryKey: true,
+			},
+			title: {
+				type: DataTypes.STRING(50),
+				allowNull: false,
+			},
+			content: {
+				type: DataTypes.STRING(500),
+			},
+			userUuid: {
+				type: DataTypes.STRING(50),
+				allowNull: false,
+				field: 'user_uuid',
+			},
+			school: {
+				type: DataTypes.STRING(20),
+				allowNull: false,
+			},
+			createdAt: {
+				type: DataTypes.DATE,
+				defaultValue: DataTypes.NOW,
+				field: 'created_at',
+			},
+		},
+		{
+			charset: 'utf8',
+			freezeTableName: true,
+			tableName: 'notice',
+		},
+	);
+	return notice;
+};
 
-module.exports = Notice
+module.exports = Notice;
