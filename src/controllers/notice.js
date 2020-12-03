cb1438a247c08c9f3251322d104068a126b259f2
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-12-03 19:38:09 +0900

    Add define getnotice function

---
+++ src/controllers/notice.js
@@ -0,0 +1,13 @@
+const getLatestNotice = (req, res, next) => {
+	return;
+};
+
+const getNoticeList = (req, res, next) => {
+	return;
+};
+
+const getDetailNotice = (req, res, next) => {
+	return;
+};
+
+module.exports = { getLatestNotice, getNoticeList, getDetailNotice };
--- src/routes/notice.js
+++ src/routes/notice.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+const {
+	getLatestNotice,
+	getNoticeList,
+	getDetailNotice,
+} = require('../controllers/notice');
+const authMiddleware = require('../middlewares/auth');
+const tryCatchMiddleware = require('../middlewares/tryCatch');
 
-router.get('/'); //get notice querystr : <page:int>
-router.get('/:id'); //detail notice
-router.get('/latest'); //loatest notice
+router.get('/', authMiddleware, tryCatchMiddleware(getNoticeList)); //get notice querystr : <page:int>
+router.get('/:id', authMiddleware, tryCatchMiddleware(getDetailNotice)); //detail notice
+router.get('/latest', authMiddleware, tryCatchMiddleware(getLatestNotice)); //loatest notice
 
 module.exports = router;
