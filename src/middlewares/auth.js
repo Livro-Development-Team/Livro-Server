cd3330c197c2acd83a41f61a0893b443cd48f5cc
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-12-03 18:48:52 +0900

    Add jwt authorization middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,10 +1,14 @@
-// jwt authenticaion 확인
-
 const jwt = require('jsonwebtoken');
 
-const authMiddleWare = async (req, res, next) => {
+const authMiddleware = async (req, res, next) => {
 	const token = req.headers['authorization'];
 	if (!token) res.status(401).json({ message: 'token required' });
 	const bearer = token.split('Bearer ')[1];
-	await jwt.verify(bearer);
+	await jwt.verify(bearer, req.app.get('jwt-secret'), (err, decoded) => {
+		if (err) res.status(403).json({ message: err.message });
+		req['decoded'] = decoded;
+		next();
+	});
 };
+
+module.exports = authMiddleware;
