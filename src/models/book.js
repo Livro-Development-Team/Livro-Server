a35d419fdd7118dd6f4be61178fbb345a4f3bbba
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-12-03 19:05:09 +0900

    Fix return model definition

--- src/models/book.js
+++ src/models/book.js
@@ -1,50 +1,53 @@
-const Loan = require("./loan")
+const Loan = require('./loan');
 
 const Book = (sequelize, DataTypes) => {
-    const book = sequelize.define('book', {
-        id : {
-            type: DataTypes.STRING(10),
-            primaryKey: true,
-        },
-        title: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-        },
-        author: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-        },
-        publisher: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-        },
-        category: {
-            type: DataTypes.STRING(10),
-            allowNull: false,
-        },
-        location: {
-            type: DataTypes.STRING(40),
-            allowNull: false,
-        },
-        image: {
-            type: DataTypes.STRING(200),
-            defaultValue:
-            "https://images-na.ssl-images-amazon.com/images/I/417efBUAHCL._SX317_BO1,204,203,200_.jpg",
-        },
-        school: {
-            type: DataTypes.STRING(20),
-            allowNull: false,
-        }
-    }, 
-    {
-        charset: 'utf8',
-        freezeTableName: true,
-        tableName: 'book',
-    });
-    book.associate = () => {
-        Book.hasMany(Loan, { foreignKey: "bookId", sourceKey: "id" })
-        Loan.belongsTo(Book, { foreignKey: "bookId" })
-    }
-    return book
-}
-module.exports = Book
+	const book = sequelize.define(
+		'book',
+		{
+			id: {
+				type: DataTypes.STRING(10),
+				primaryKey: true,
+			},
+			title: {
+				type: DataTypes.STRING(50),
+				allowNull: false,
+			},
+			author: {
+				type: DataTypes.STRING(50),
+				allowNull: false,
+			},
+			publisher: {
+				type: DataTypes.STRING(50),
+				allowNull: false,
+			},
+			category: {
+				type: DataTypes.STRING(10),
+				allowNull: false,
+			},
+			location: {
+				type: DataTypes.STRING(40),
+				allowNull: false,
+			},
+			image: {
+				type: DataTypes.STRING(200),
+				defaultValue:
+					'https://images-na.ssl-images-amazon.com/images/I/417efBUAHCL._SX317_BO1,204,203,200_.jpg',
+			},
+			school: {
+				type: DataTypes.STRING(20),
+				allowNull: false,
+			},
+		},
+		{
+			charset: 'utf8',
+			freezeTableName: true,
+			tableName: 'book',
+		},
+	);
+	book.associate = () => {
+		Book.hasMany(Loan, { foreignKey: 'bookId', sourceKey: 'id' });
+		Loan.belongsTo(Book, { foreignKey: 'bookId' });
+	};
+	return book;
+};
+module.exports = Book;
--- src/models/loan.js
+++ src/models/loan.js
@@ -1,39 +1,42 @@
 const Loan = (sequelize, DataTypes) => {
-    sequelize.define('loan', {
-        uuid: {
-            type: DataTypes.STRING(50),
-            primaryKey: true
-        },
-        userUuid: {
-           type: DataTypes.STRING(50),
-           allowNull: false,
-           field: "user_uuid",
-        },
-        bookId: {
-            type: DataTypes.STRING(10),
-            allowNull: false,
-            field: "book_id",
-        },
-        school: {
-            type: DataTypes.STRING(20),
-            allowNull: false,
-        },
-        createdAt: {
-            type: DataTypes.DATE,
-            defaultValue: DataTypes.NOW,
-            field: "created_at",
-        },
-        deletedAt: {
-            type: DataTypes.STRING(30),
-            field: "deleted_at"
-        },
-    },
-    {
-        charset: 'utf8',
-        freezeTableName: true,
-        tableName: 'loan',
-    }
-    )
-}
+	const loan = sequelize.define(
+		'loan',
+		{
+			uuid: {
+				type: DataTypes.STRING(50),
+				primaryKey: true,
+			},
+			userUuid: {
+				type: DataTypes.STRING(50),
+				allowNull: false,
+				field: 'user_uuid',
+			},
+			bookId: {
+				type: DataTypes.STRING(10),
+				allowNull: false,
+				field: 'book_id',
+			},
+			school: {
+				type: DataTypes.STRING(20),
+				allowNull: false,
+			},
+			createdAt: {
+				type: DataTypes.DATE,
+				defaultValue: DataTypes.NOW,
+				field: 'created_at',
+			},
+			deletedAt: {
+				type: DataTypes.STRING(30),
+				field: 'deleted_at',
+			},
+		},
+		{
+			charset: 'utf8',
+			freezeTableName: true,
+			tableName: 'loan',
+		},
+	);
+	return loan;
+};
 
-module.exports = Loan
+module.exports = Loan;
